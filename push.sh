4143cf804bf1a33b9b025716617b5ba9c2aa450f
LiTangWang6 <15970579357@163.com>
2023-09-14 10:59:54 +0800

    zidong

---
+++ push.sh
@@ -0,0 +1,6 @@
+git config --global user.name 'LiTangWang6'
+git config --global user.email '15970579357@163.com'
+git config --global http.sslverify false
+git add .
+git commit -m zidong
+git push
